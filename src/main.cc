30f4bbb82d19e88986df6309ce04b2026b7c8155
Amir Nourinia <amir.nourinia@gmail.com>
2020-07-27 22:04:42 +0200

    update

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,6 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main()
 {
@@ -18,6 +20,12 @@ int main()
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    if(glewInit() != GLEW_OK)
+        std::cout << "Error glewInit" <<std::endl;
+
+    // To print out the opengl version with the vendor.
+    std::cout << glGetString(GL_VERSION) << std::endl;
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
